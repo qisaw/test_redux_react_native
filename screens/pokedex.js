2ece1d13aef2b46c555ef77c55b4bc18ad9496f7
Wasiq Kashkari <w.kashkari@outlook.com>
2018-06-26 11:56:42 +1200

    add pokedex screen

---
+++ screens/pokedex.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Pokedex extends Component {
+  render() {
+    return (<Text> Hi Pokedex </Text>);
+  }
+}
